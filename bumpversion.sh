6b9a63afe362278eea61de7afa01101339123e7c
Franklin Wise <franklin@krave.io>
2015-05-05 16:40:46 -0700

    another update to bumpversion

--- bumpversion.sh
+++ bumpversion.sh
@@ -27,10 +27,10 @@ if [ -z "$(git status --porcelain)" ]; then
   #git merge develop
   #git checkout develop
   bumpversion $1
-  #git add .
-  #git commit -m 'bumpversion'
-  #git push --all
-  #git push --tags
+  git add .
+  git commit -m 'bumpversion'
+  git push --all
+  git push --tags
 else 
   # Uncommitted changes
   echo 'uncommitted changes exist, must be run on a clean repository'
